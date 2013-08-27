b3d599d424ce69665b2589bc1b35e43ee1c06666
JeanMouloude <lucas.pluvinage@gmail.com>
2013-08-27 21:11:52 +0200

    Implémentation du YoloSwag decoder à vérifier

--- InPut/yolodecoder.cpp
+++ InPut/yolodecoder.cpp
@@ -3,3 +3,31 @@
 YoloDecoder::YoloDecoder()
 {
 }
+
+QString YoloDecoder::splitCharacter() {
+    char c[0];
+    c[0] = 0xFF;
+    return QString::fromLocal8Bit(c);
+}
+
+void YoloDecoder::decodeString(QString &str) {
+    // #c'estpaspeucher.
+    QByteArray data = str.toLocal8Bit();
+    if(data.size() < 10)
+        return;
+
+    char checkSum = data.at(9);
+
+    char check = 0x00;
+    for(int i=0; i<9; i++) {
+        check += data.at(i);
+    }
+    check >> 1;
+
+    if(check == checkSum) {
+        for(int i=0; i<9; i++) {
+            emit newValue(i, 0, data.at(i));
+        }
+    }
+
+}
--- InPut/yolodecoder.h
+++ InPut/yolodecoder.h
@@ -1,10 +1,20 @@
 #ifndef YOLODECODER_H
 #define YOLODECODER_H
+#include <QString>
+#include <QtGui>
 
-class YoloDecoder
+class YoloDecoder : public QObject
 {
 public:
     YoloDecoder();
+    QString splitCharacter();
+    void decodeString(QString &str);
+
+signals:
+    void newValue(int id_capteur, int id_valeur, double valeur);
+private:
+    Q_OBJECT
+
 };
 
 #endif // YOLODECODER_H
