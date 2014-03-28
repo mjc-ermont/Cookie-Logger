ccfcfffab7fa1e0f69893110b6b4bbb220bc8be3
konfiot <arthurtoussaint@free.fr>
2014-03-28 22:15:51 +0100

    Encore des dépendances

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ then
 	make
 elif [ $OS == "WIN" ];
 then
-	sudo apt-get install autopoint
+	sudo apt-get install autopoint libtool
 	git clone https://github.com/mxe/mxe.git &&
 	cd mxe &&
 	make qt &&
