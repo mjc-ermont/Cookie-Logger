c994b0b485c97faf38e17a41683f37cbcf60c688
konfiot <arthurtoussaint@free.fr>
2014-03-28 19:33:00 +0100

    On teste la cross compilation vers windows

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd ChronoReader &&
+git pull origin master &&
+git submodule init &&
+git submodule update &&
+cd .. &&
+
+if [$OS -eq "LINUX"]
+then
+	sudo apt-get install qt4-dev-tools libqjson-dev libqwt-dev &&
+	git clone git://gitorious.org/qt/qtserialport.git &&
+	cd qtserialport &&
+	qmake qtserialport.pro &&
+	make &&
+	sudo make install &&
+	cd ..
+	qmake Logger21.pro &&
+	make
+elif [$OS -eq "WIN"]
+then
+	git clone https://github.com/mxe/mxe.git &&
+	cd mxe &&
+	make qt &&
+	make qwt &&
+	make qjson &&
+	make qtserialport &&	
+	cd .. &&
+	export PATH=$TRAVIS_BUILD_DIR/mxe/usr/bin:$PATH &&
+	mxe/usr/i686-pc-mingw32/qt/bin/qmake Logger21.pro &&
+	make
+
