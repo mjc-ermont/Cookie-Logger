d661a49fdff0c3b1d05f713c8576ad4421e0c7f9
Arthur <konfiot@users.noreply.github.com>
2014-04-03 20:39:53 +0200

    J'avais oublié d'installer des dépendances

--- build.sh
+++ build.sh
@@ -19,6 +19,7 @@ then
 	make
 elif [ $OS == "WIN" ];
 then
+	sudo apt-get install autopoint intltool liblzma-dev &
 	wget https://dl.dropboxusercontent.com/s/bhhvvbmfzy207rp/mxe.tar.xz && 
 	git clone https://github.com/jnovy/pxz && 
 	cd pxz && 
@@ -27,7 +28,6 @@ then
 	cd .. &&
 	pxz mxe -d mxe.tar.xz &&
 	tar -zf mxe.tar &&
-	sudo apt-get install autopoint intltool &&
 	export PATH=$TRAVIS_BUILD_DIR/mxe/usr/bin:$PATH &&
 	mxe/usr/i686-pc-mingw32/qt/bin/qmake Logger21.pro &&
 	make
