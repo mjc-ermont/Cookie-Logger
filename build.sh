8aae178d27e932055b812eb32ccfaa49b7a514e0
Arthur <konfiot@users.noreply.github.com>
2014-04-04 22:17:07 +0200

    Bon bah en fait on est obligé de tout rebuild ...

--- build.sh
+++ build.sh
@@ -20,16 +20,13 @@ then
 elif [ $OS == "WIN" ];
 then
 	sudo apt-get install autopoint intltool liblzma-dev &&
-	wget https://dl.dropboxusercontent.com/s/bhhvvbmfzy207rp/mxe.tar.xz && 
-	git clone https://github.com/jnovy/pxz && 
-	cd pxz && 
-	make &&
-	sudo make install &&
+	git clone https://github.com/mxe/mxe.git &&
+	cd mxe &&
+	curl http://arthurtoussaint.free.fr/qtserialport.patch | patch
+	curl http://arthurtoussaint.free.fr/qwt.patch | patch
+	make --jobs=8 qt qwt_qt4 qjson qtserialport &&
 	cd .. &&
-	pxz -d mxe.tar.xz &&
-	tar xf mxe.tar &&
 	export PATH=$TRAVIS_BUILD_DIR/mxe/usr/bin:$PATH &&
-	export QMAKESPEC=$TRAVIS_BUILD_DIR/mxe/usr/i686-w64-mingw32.shared/qt/mkspecs/win32-g++ &&
 	mxe/usr/i686-w64-mingw32.shared/qt/bin/qmake Logger21.pro &&
 	make
 fi
