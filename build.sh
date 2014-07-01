46121a3291a3e4b31b7181c4a607a93025832536
konfiot <arthurtoussaint@free.fr>
2014-07-01 21:09:17 +0200

    Ca devrait être bon

--- build.sh
+++ build.sh
@@ -21,18 +21,19 @@ then
 elif [ $OS == "WIN" ];
 then
 	sudo apt-get install autopoint intltool gperf cmake scons liblzma-dev &&
-	git clone https://github.com/jnovy/pxz && 
-	cd pxz &&
-	make &&
-	sudo make install && 
-	cd .. &&
-	wget https://dl.dropboxusercontent.com/s/bhhvvbmfzy207rp/mxe.tar.xz &&
+	if [ $ARCH == "i686" ]
+	then
+		wget https://dl.dropboxusercontent.com/s/do0ufqeziw8d2qo/mxe_i686.tar.xz &&
+	elif [ $ARCH == "x86_64" ]
+		wget https://dl.dropboxusercontent.com/s/r2frnt3b68heh80/mxe_x86_64.tar.xz &&	
+	fi
 	echo "Extracting" &&
-	pxz -d mxe.tar.xz &&
+	pxz -d mxe_$ARCH.tar.xz &&
 	echo "Detarring" &&
-	tar -xf mxe.tar &&
-	export PATH=/home/travis/mjc-ermont/Cookie-Logger/mxe/usr/bin:$PATH &&
+	tar -xf mxe_$ARCH.tar &&
+	sudo cp -r mxe /opt
+	export PATH=/opt/mxe/usr/bin:$PATH &&
 	echo "Building" &&
-	/home/travis/mjc-ermont/Cookie-Logger/mxe/usr/i686-pc-mingw32.shared/qt/bin/qmake Logger21.pro &&
+	/opt/mxe/usr/$ARCH-w64-mingw32.shared/qt/bin/qmake Logger21.pro &&
 	make
 fi
