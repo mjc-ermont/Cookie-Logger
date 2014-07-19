9edc47cc7387fa36197ddf8fcc3a8e18580ad629
Arthur <konfiot@users.noreply.github.com>
2014-07-19 17:07:06 +0200

    Il manquait zlib

--- build.sh
+++ build.sh
@@ -60,7 +60,7 @@ then
 	/opt/mxe/usr/$ARCH-w64-mingw32.shared/qt/bin/qmake Logger21.pro &&
 	make &&
 	mkdir build &&
-	cp release/Logger21.exe /opt/mxe/usr/$ARCH-w64-mingw32.shared/bin/libqjson.dll /opt/mxe/usr/$ARCH-w64-mingw32.shared/qt/lib/{qwt,QtCore4,QtGui4,QtSql4,QtSerialPort,QtWebKit4,QtNetwork4}.dll  build/ &&
+	cp release/Logger21.exe /opt/mxe/usr/$ARCH-w64-mingw32.shared/bin/{libqjson,zlib1}.dll /opt/mxe/usr/$ARCH-w64-mingw32.shared/qt/lib/{qwt,QtCore4,QtGui4,QtSql4,QtSerialPort,QtWebKit4,QtNetwork4}.dll  build/ &&
 	zip -r Logger21-$OS-$ARCH.zip build/
 fi
 
