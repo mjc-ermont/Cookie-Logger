92da4f30df68fa1e0ee17ba05073cccd9fcf0783
Arthur <konfiot@users.noreply.github.com>
2014-07-20 22:26:14 +0200

    Si on rajoute O3, faut enlever 02 ...

--- build.sh
+++ build.sh
@@ -50,7 +50,7 @@ then
 	sudo mv /opt/mxe/usr/$ARCH-w64-mingw32.shared/qt/include/{qw*.h,qwt/}
 	export PATH=/opt/mxe/usr/bin:$PATH &&
 	echo "Building" &&
-	/opt/mxe/usr/$ARCH-w64-mingw32.shared/qt/bin/qmake Logger21.pro QMAKE_CXXFLAGS+="-O3" &&
+	/opt/mxe/usr/$ARCH-w64-mingw32.shared/qt/bin/qmake Logger21.pro QMAKE_CXXFLAGS+="-O3" QMAKE_CXXFLAGS-="-O2" &&
 	make &&
 	mkdir build &&
 	cd build &&
