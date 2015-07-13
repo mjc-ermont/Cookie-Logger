e887d4e30c6fa6614ffdf3bdb8add2a9d3b7cb7e
konfiot <arthurtoussaint@free.fr>
2015-07-13 15:15:06 +0200

    Test

--- build.sh
+++ build.sh
@@ -6,7 +6,10 @@ if [ $OS == "LINUX" ];
 then
 	if [ $ARCH == "i686" ];
 	then
-		sudo apt-get install qt4-dev-tools:i386 libqjson-dev:i386 libqwt-dev:i386 libc6-dev-i386 gcc-multilib g++-multilib python3.2 python3.2-dev python-numpy-dev &&
+		sudo dpkg --add-architecture i386
+		sudo apt-get update
+		sudo apt-get install ia32-libs
+		sudo apt-get install qt4-dev-tools:i386 libqjson-dev:i386 libqwt-dev:i386 libc6-dev-i386 gcc-multilib g++-multilib python3.2:i386 python3.2-dev:i386 python-numpy-dev &&
 		git clone git://code.qt.io/qt/qtserialport.git &&
 		cd qtserialport &&
 		git checkout qt4-dev
