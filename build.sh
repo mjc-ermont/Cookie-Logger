2cf6499b98332da00e2877ad3e12c45fa43bbb62
Rémi <remim.dupre@gmail.com>
2015-07-21 22:07:17 +0200

    Travis : meurs

--- build.sh
+++ build.sh
@@ -9,9 +9,8 @@ then
 		cd ..
 		sudo apt-get install autopoint qemu qemu-system qemu-kvm build-essential openssh-server
 		sudo /etc/init.d/ssh start
-		wget http://mirrors.kernel.org/archlinux/iso/2015.07.01/archlinux-bootstrap-2015.07.01-i686.tar.gz
-		tar -xvf archlinux-bootstrap-2015.07.01-i686.tar.gz
-		qemu-system-i386 -no-kvm -cdrom archlinux-bootstrap-2015.07.01-i686.iso -m 1024 -nographic -no-reboot -redir tcp:5555::22 &
+		wget http://mirrors.kernel.org/archlinux/iso/2015.07.01/archlinux-2015.07.01-dual.iso
+		qemu-system-i386 -no-kvm -cdrom archlinux-2015.07.01-dual.iso -m 1024 -nographic -no-reboot -redir tcp:5555::22 &
 		sleep 60
 		echo "Connection au ssh"
 		scp -r -P 5555 ./Cookie-Logger root@localhost:./Cookie-Logger
