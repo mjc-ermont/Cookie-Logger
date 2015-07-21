da3647648c1e61e85cd8d0b4b6e968649e936908
Rémi <remim.dupre@gmail.com>
2015-07-21 14:21:24 +0100

    Tentive de build de python

--- build.sh
+++ build.sh
@@ -9,7 +9,12 @@ then
 		sudo dpkg --add-architecture i386
 		sudo apt-get update
 		sudo apt-get install ia32-libs
-		sudo apt-get install qt4-dev-tools:i386 libqjson-dev:i386 libqwt-dev:i386 libc6-dev-i386 gcc-multilib g++-multilib python3.2:i386 python3.2-dev:i386 python-numpy-dev ia32-libs-multiarch mime-support:i386 libssl-dev:i386 libexpat1-dev:i386 &&
+		sudo apt-get build-dep python3.2:i386 python3.2-dev:i386
+		wget https://www.python.org/ftp/python/3.4.3/Python-3.4.3.tgz &&
+		tar -xvf Python-3.4.3.tgz &&
+		cd Python-3.4.3 &&
+		./configure && make && sudo make install &&
+		sudo apt-get install qt4-dev-tools:i386 libqjson-dev:i386 libqwt-dev:i386 libc6-dev-i386 gcc-multilib g++-multilib python-numpy-dev &&
 		git clone git://code.qt.io/qt/qtserialport.git &&
 		cd qtserialport &&
 		git checkout qt4-dev
